4c1483339c46dc9492c36ff5e48c848fb2b5c0ab
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2025-02-24 14:28:03 +0000

    jwt auth set to false to allow local testing

--- drf_testing/settings.py
+++ drf_testing/settings.py
@@ -50,6 +50,8 @@ JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
+if 'DEV' in os.environ:
+    JWT_AUTH_SECURE = False
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
