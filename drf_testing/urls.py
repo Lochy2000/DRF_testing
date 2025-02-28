88646ad776993006416aac731bcd5878d1dd8578
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2025-02-28 12:27:27 +0000

    urls not working

--- drf_testing/urls.py
+++ drf_testing/urls.py
@@ -1,22 +1,7 @@
-"""drf_api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from .views import root_route, logout_route
-from dj_rest_auth.views import refresh_jwt_token
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('', root_route),
@@ -28,7 +13,7 @@ urlpatterns = [
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
-    path('dj-rest-auth/token/refresh/', refresh_jwt_token, name='token_refresh'),
+    path('dj-rest-auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
