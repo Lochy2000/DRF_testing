00c95b83c6dfe5d7e114824f2d6271848a5bbd3e
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2025-03-04 14:16:43 +0000

    finally user default profile working

--- profiles/models.py
+++ profiles/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-
+from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,9 +9,9 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', 
-        default='https://res.cloudinary.com/dpw2txejq/image/upload/default_profile_ju9xum'
+    image = CloudinaryField(
+        'image', 
+        default='default_profile_ju9xum'  # use only the public ID
     )
 
     class Meta:
