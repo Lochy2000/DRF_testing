15356dac13ac3c3d90576bb7cb489a2b16bfe087
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2025-03-04 11:32:14 +0000

    udpate to cloudinary config

--- drf_testing/settings.py
+++ drf_testing/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 import re
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 if os.path.exists('env.py'):
     import env
@@ -21,6 +24,14 @@ if os.path.exists('env.py'):
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
+
+# Configure Cloudinary
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+    secure=True
+)
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- posts/models.py
+++ posts/models.py
@@ -29,7 +29,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_rgq6aq', blank=True
+        upload_to='images/', default='../default_post_o0lbny', blank=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
